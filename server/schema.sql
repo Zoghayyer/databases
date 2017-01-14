4b56c289a9b689d3ada704b5f0e79d061b444cc4
zoghayyer <azzoghayyer@gmail.com>
2017-01-13 20:08:56 -0800

    created tables!

--- server/schema.sql
+++ server/schema.sql
@@ -4,9 +4,22 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  text VARCHAR(110),
+  user_id INT UNSIGNED NOT NULL,
+  message_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  FOREIGN KEY (user_id) REFERENCES users(user_id);
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users {
+  user_name VARCHAR(30) DEFAULT 'anonymous',
+  user_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY
+};
+
+CREATE TABLE rooms {
+  room_name VARCHAR(30) DEFAULT 'lobby', 
+  room_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY
+};
 
 
 
@@ -14,4 +27,3 @@ CREATE TABLE messages (
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
-
