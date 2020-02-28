3f6151a3ffae195351e7dc5fe14915abb621cb4b
Ahmed Zoghayyer <azzoghayyer@gmail.com>
2020-02-27 20:18:15 -0800

    Update schema.sql
    Update comments

--- server/schema.sql
+++ server/schema.sql
@@ -12,18 +12,8 @@ CREATE TABLE messages (
   PRIMARY KEY (ID)
 );
 
-/* Create other tables and define schemas for them here! */
-
-
 CREATE TABLE users (
   id        int    NOT NULL AUTO_INCREMENT,
   username  varchar(40)   NOT NULL,
   PRIMARY KEY (ID)
 );
-
--- ALTER TABLE `messages` ADD FOREIGN KEY (user) REFERENCES `users` (`user_id`);
-
--- FOREIGN KEY (user) REFERENCES users(user_id)
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
