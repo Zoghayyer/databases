8721c38e1d714d05b25834878ce093f2b9b85f8f
Ahmed Zoghayyer <azzoghayyer@gmail.com>
2020-02-27 20:18:56 -0800

    Update routes.js
    Fix eslint errors

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,7 @@
 var controller = require('./controllers');
 var router = require('express').Router();
 
-//Connect controller methods to their corresponding routes
+// Connect controller methods to their corresponding routes
 router.get('/messages', controller.messages.get);
 
 router.post('/messages', controller.messages.post);
@@ -9,6 +9,5 @@ router.post('/messages', controller.messages.post);
 router.get('/users', controller.users.get);
 
 router.post('/users', controller.users.post);
-
 
 module.exports = router;
