16656843ac5f4b40a656602886573bb0de068df0
Ahmed Zoghayyer <azzoghayyer@gmail.com>
2020-02-27 20:24:23 -0800

    Update index.js
    Fix eslint errors

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,24 +3,17 @@ var models = require('../models');
 module.exports = {
 
   messages: {
-    get: function (req, res) {
-
-    }, // a function which handles a get request for all messages
     post: function (req, res) {
       var body = '';
-      // console.log('Messages POST: ' + JSON.parse(req.body));
       req.on('data', function(data) {
         body += data;
       });
       req.on('end', function(data) {
-        console.log(JSON.parse(body));
-
         var params = [req.body.username,
         req.body.message,
         req.body.roomname];
 
         models.messages.post(params, function(err, results) {
-
           if (err) {
             throw error;
             console.log('Error Posting to Server: ', err);
@@ -30,11 +23,10 @@ module.exports = {
         });
 
       });
-    } // a function which handles posting a message to the database
+    }
   },
 
   users: {
-    // Ditto as above
     get: function (req, res) {
       models.users.get(function(err, results) {
         if (err) { console.log('get controller: error'); }
@@ -42,9 +34,7 @@ module.exports = {
       });
     },
     post: function (req, res) {
-      console.log('please come here', req.body.user_name);
-
-      var params = [req.body.username]; //username
+      var params = [req.body.username];
       models.users.post(params, function(err, results ) {
         if (err) {
           console.log('Error Posting to Server: ', err);
